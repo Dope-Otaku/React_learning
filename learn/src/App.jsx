af515014d6f55e4d46d2078e7c70faf3b428ddcd
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-16 23:45:46 +0530

    added a footer just for clarity

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -4,6 +4,7 @@ import Main from "./learning_comp/Routing/Main";
 import About from "./learning_comp/Routing/About";
 import Contact from "./learning_comp/Routing/Contact";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./learning_comp/Footer";
 // import Props from "./learning_comp/Props";
 // import Home from "./learning_comp/Home";
 // import TodoL from "./learning_comp/Todo/TodoL";
@@ -15,13 +16,14 @@ function App() {
       {/* <TodoL /> */}
       {/* <Home /> */}
       {/* <Props /> */}
-      
+
       <Routes>
-        <Route path="/" element={<Main />}/>
-        <Route path="/contact" element={<Contact />}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-      
+
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ learn/src/learning_comp/Footer.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <span>All right reserved for Dope_Otaku</span>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
