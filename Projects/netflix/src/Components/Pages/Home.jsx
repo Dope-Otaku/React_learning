939e5e4acf48698dfd08276368a8dcd03f57f7b5
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-17 22:49:38 +0530

    created the upcoming movies folder

--- Projects/netflix/src/Components/Pages/Home.jsx
+++ Projects/netflix/src/Components/Pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
 import "./css/Home.css";
+import axios from "axios";
+
+const apiKey = "7e5122f42b3d47b2f9c1deaf4e1d2214";
+const url = "https://api.themoviedb.org/3";
+const imgUrl = "https://image.tmdb.org/t/p/original/";
+// const upcoming = upcoming;
 
 const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
 
@@ -13,19 +20,33 @@ const Row = ({
   <div className="row">
     <h2>{title}</h2>
     <div className="innerWal">
-      {arr.map((item) => (
-        <Card key={item.img} img={item.img} />
+      {arr.map((item, index) => (
+        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
       ))}
     </div>
   </div>
 );
 
 const Home = () => {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+
+  useEffect(() => {
+    const upcomingData = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/movie/upcoming?api_key=${apiKey}`);
+      // console.log(results);
+      setUpcomingMovies(results);
+    };
+    upcomingData();
+  }, []);
+
   return (
     <section className="home">
       <div className="banner"></div>
       {/* <h1>Popular on Net</h1> */}
-      <Row title={"Popular on Netflix"} />
+
+      <Row title={"Upcoming Movies"} arr={upcomingMovies} />
       <Row title={"Recently Watched"} />
       <Row title={"New List"} />
     </section>
