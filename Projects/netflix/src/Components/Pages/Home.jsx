e3cbf06a36932317ab3096f5b4155e4d97a0012c
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-17 13:48:46 +0530

    the homepage is stticaly done

--- Projects/netflix/src/Components/Pages/Home.jsx
+++ Projects/netflix/src/Components/Pages/Home.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import "./css/Home.css";
+
+const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
+
+const Row = ({
+  title,
+  arr = [
+    {
+      img: "http://media.comicbook.com/201/03/avengers-infinity-war-poster-1093756.jpeg",
+    },
+  ],
+}) => (
+  <div className="row">
+    <h2>{title}</h2>
+    <div className="innerWal">
+      {arr.map((item) => (
+        <Card key={item.img} img={item.img} />
+      ))}
+    </div>
+  </div>
+);
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="banner"></div>
+      {/* <h1>Popular on Net</h1> */}
+      <Row title={"Popular on Netflix"} />
+      <Row title={"Recently Watched"} />
+      <Row title={"New List"} />
+    </section>
+  );
 };
 
 export default Home;
