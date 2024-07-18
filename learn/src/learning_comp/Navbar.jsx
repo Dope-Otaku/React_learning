663e95f1bb448e6a657741ff461206413b90b1a9
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-18 13:26:44 +0530

    commited

--- learn/src/learning_comp/Navbar.jsx
+++ learn/src/learning_comp/Navbar.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 function Navbar() {
+  const nai = useNavigate();
   return (
     <>
       <nav className="navbarStyling">
         <h1>React Learning Curve</h1>
         <button>
-          <a>Login In</a>
+          <a
+            onClick={() => {
+              nai("/play");
+            }}
+          >
+            Login In
+          </a>
         </button>
       </nav>
     </>
