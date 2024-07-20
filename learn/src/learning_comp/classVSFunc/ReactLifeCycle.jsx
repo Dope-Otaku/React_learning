be1d93419a4c037d56f367edc4b58a53a9f911fe
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-20 21:42:49 +0530

    added a comment in componentDidUpdate

--- learn/src/learning_comp/classVSFunc/ReactLifeCycle.jsx
+++ learn/src/learning_comp/classVSFunc/ReactLifeCycle.jsx
@@ -12,16 +12,47 @@ setTimeout(() => {
 class ReactLifeCycle extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
 
     // this.componentDidMount = this.componentDidMount.bind(this);
+    this.increment = this.increment.bind(this);
   }
+
+  increment() {
+    this.setState((prev) => {
+      return {
+        count: prev.count + 1,
+      };
+    });
+  }
+
+  //   increment() {   this will not work as we have no prev state to remove it with
+  //     this.setState(() => {
+  //       return {
+  //         count: this.count + 1,
+  //       };
+  //     });
+  //   }
+
   componentDidMount() {
     setTimeout(() => {
-      console.log("called timer");
+      console.log("componentDidMount timer");
+    }, 3000);
+  }
+  componentDidUpdate() {
+    setTimeout(() => {
+      console.log("componentDidUpdate timer");
     }, 3000);
   }
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.increment}>Increase</button>
+      </div>
+    );
   }
 }
 
