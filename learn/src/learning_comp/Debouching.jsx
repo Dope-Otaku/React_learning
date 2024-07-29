9405d2cae0446d8694cd821b2a501b1975c4e9fd
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-29 21:21:27 +0530

    learned debouncing timeout

--- learn/src/learning_comp/Debouching.jsx
+++ learn/src/learning_comp/Debouching.jsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 
 const Home = lazy(() => import("./Home"));
 const Footer = lazy(() => import("./Footer"));
@@ -6,3 +6,17 @@ const Navbar = lazy(() => import("./Navbar"));
 
 // -----------------------------------------------------------------Debouching & code splitting -----check in source
 // the above text are used for lazy loading so that the component is not always loaded incase of heavy application it will ijncrease respojnse time which will increase cost
+
+{
+  /* <Router>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Footer" element={<Footer />} />
+      <Route path="/Navbar" element={<Navbar />} />
+    </Routes>
+  </Suspense>
+</Router>; */
+}
+
+//this function {suspense} creates a pre loaded skeleteon feture so that if a page is not loaded it will show that before everything
